916767edf5fe618e5c9b773581680dcb137cddef
fredThem <fdesmarais.them@gmail.com>
2021-05-09 08:39:56 -0400

    Run the React on Rails Generator

--- Gemfile
+++ Gemfile
@@ -53,4 +53,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "webpacker", "~> 5.3"
 gem 'mini_racer', platforms: :ruby
-gem 'mini_racer', platforms: :ruby
--- app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.jsx
@@ -1,25 +1,25 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React from 'react';
 
-const HelloWorld = (props) => {
-  const [name, setName] = useState(props.name);
-
-  return (
-    <div>
-      <h3>Hello, {name}!</h3>
-      <hr />
-      <form>
-        <label htmlFor="name">
-          Say hello to:
-          <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-      </form>
-    </div>
-  );
-};
+const HelloWorld = ({ name, updateName }) => (
+  <div>
+    <h3>
+      Hello,
+      {name}!
+    </h3>
+    <hr />
+    <form>
+      <label htmlFor="name">
+        Say hello to:
+        <input id="name" type="text" value={name} onChange={(e) => updateName(e.target.value)} />
+      </label>
+    </form>
+  </div>
+);
 
 HelloWorld.propTypes = {
-  name: PropTypes.string.isRequired, // this is passed from the Rails view
+  name: PropTypes.string.isRequired,
+  updateName: PropTypes.func.isRequired,
 };
 
 export default HelloWorld;
